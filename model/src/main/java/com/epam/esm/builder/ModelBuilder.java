a4b3662bb5e3320ab1e3d2a7cacc6ed531db9f1c
Dayanch_Ishangulyev_Linux <ishankulievdayantch@yandex.ru>
2022-04-26 01:15:38 +0300

    full move module_2 to module_3

--- model/src/main/java/com/epam/esm/builder/ModelBuilder.java
+++
@@ -1,5 +0,0 @@
-package com.epam.esm.builder;
-
-public interface ModelBuilder {
-    Object build();
-}
--- persistence/src/main/java/com/epam/esm/dao/CrudDao.java
+++
@@ -1,8 +0,0 @@
-package com.epam.esm.dao;
-
-
-import com.epam.esm.exception.DaoException;
-
-public interface CrudDao<T,K> extends CrdDao<T,K> {
-    void update(T entity, K id) throws DaoException;
-}
