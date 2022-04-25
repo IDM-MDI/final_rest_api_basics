3f208e517c0695cb268bb539e14f7ca5f25d0926
IDM-MDI <ishankulievdayantch@yandex.ru>
2022-04-25 14:02:29 +0300

    add 3 module

---
+++ src/main/java/com/epam/esm/final_rest_api_basics/FinalRestApiBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.esm.final_rest_api_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalRestApiBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalRestApiBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/esm/final_rest_api_basics/FinalRestApiBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.esm.final_rest_api_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalRestApiBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
